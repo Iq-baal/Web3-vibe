d758c28ae47810e386cd5715ce244664fbed6801
Iq-baal <asherpride112@gmail.com>
2025-09-05 18:14:35 +0100

    Initial commit - first Web3 vibe project

---
+++ vibe.js
@@ -0,0 +1,15 @@
+// 🌐 My First Web3 Vibe Project
+console.log("Welcome to Web3 world!");
+
+// Mint a fake token
+let myTokens = 0;
+
+function mintToken() {
+    myTokens += 1;
+    console.log(`✨ You minted token #${myTokens}!`);
+}
+
+// Mint 3 tokens
+mintToken();
+mintToken();
+mintToken();
